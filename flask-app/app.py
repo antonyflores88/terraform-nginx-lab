9415ef0ea6e56caa00e4f96dad0e7a5af69d7b57
antonyflores88 <antonyflores2030@outlook.com>
2025-10-13 22:13:16 -0600

    app listener fix

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from your Flask inside a Terraform container!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
