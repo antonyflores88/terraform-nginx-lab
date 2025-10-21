d76df7bc6dd9822b6ee4b8c44457794a289a096a
antonyflores88 <antonyflores2030@outlook.com>
2025-10-20 21:26:09 -0600

    app modification

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from your Flask inside a Terraform container!"
+    html = """
+    <html>
+        <head>
+            <title>Tony Cloud Lab Server</title>
+        </head>
+        <body style="background-color: #1F778D">
+            <p style="text-align: center;">
+                <span style="color:#FFFFFF;">
+                    <span style="font-size:100px;">Welcome to Tony Cloud Lab Server</span>
+                </span>
+            </p>
+        </body>
+    </html>
+    WEBSITE
+    """
+    return Response(html, mimetype='text/html')
+
+@app.route('/healthz')
+def health_check():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
